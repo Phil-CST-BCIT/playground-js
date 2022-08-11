65d415adb7796615fd54ce3d64ac734b3b72ccdd
Phil <philfeiteng@gmail.com>
2022-08-11 15:30:49 -0700

    feat: add immutableMerge

---
+++ immutableMerge.js
@@ -0,0 +1,22 @@
+function immutableMerge(orig, added, removed) {
+  const newList = [...orig];
+
+  for (let e of added) {
+    if (!newList.includes(e)) {
+      newList.push(e);
+    }
+  }
+
+  const filteredList = newList.filter((item) => !removed.includes(item));
+  return filteredList
+    .sort((a, b) => b.length - a.length)
+    .sort((a, b) => b.localeCompare(a));
+}
+
+console.log(
+  immutableMerge(
+    ["one", "two", "three"],
+    ["one", "two", "five", "six"],
+    ["two", "five"]
+  )
+);
